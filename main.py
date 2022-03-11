2a4088f82a39b06d0c3659cf4d5f6e09dd97a0c4
aoshchepkov <semitro_8@mail.ru>
2022-03-11 10:21:38 +0300

    teach traffic analyzer

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from stub_data import generate_stub_data
 
 import matplotlib.pyplot as plt
 
-traffic = generate_stub_data(counts=168 * 15, intervals_in_day=15*24)
+from traffic_analyse import TrafficAnalyzer
+
+INTERVALS_IN_DAY = 15 * 24
+
+traffic = generate_stub_data(counts=168 * 15, intervals_in_day=INTERVALS_IN_DAY)
 
 # %%
 plt.title('Model of traffic during a day')
@@ -13,3 +17,7 @@ plt.ylabel('Example of Traffic, GB/s')
 plt.xlabel('Interval, 15 minute')
 plt.show()
 
+
+# %%
+traffic_analyzer = TrafficAnalyzer()
+traffic_analyzer.fit(traffic, INTERVALS_IN_DAY)
---
+++ traffic_analyse.py
@@ -0,0 +1,24 @@
+import math
+import random as rand
+import numpy as np
+
+
+class TrafficAnalyzer:
+
+    def __init__(self):
+        self.means = None
+        self.stds = None
+
+    def fit(self, data, indexes_in_day):
+        np_array = np.array(data).reshape((indexes_in_day, -1))
+        print(f'Teaching analyzer on {np_array.shape[0]} samples per day'
+              f' for {np_array.shape[1]} days')
+
+        means = np.mean(np_array, axis=1)
+        stds = np.std(np_array, axis=1)
+        print(f'{means.shape[0]} means, std taken')
+
+        self.means = means
+        self.stds = stds
+
+
